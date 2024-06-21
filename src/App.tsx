c941a134fb9c337fd7ee03af0c330df3860f8b26
João Victor Lima <victtorlima15@gmail.com>
2024-06-21 11:03:15 -0300

    stylization of the components involved in card control

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import CardList from "./Components/CardList";
+import "./Style/App.css";
 
 export type Product = {
   id: string;
--- src/Components/CardList.tsx
+++ src/Components/CardList.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { SearchByCategory } from "../App";
 import Card from "./Card";
-
-const styleContainer: React.CSSProperties = {
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr 1fr",
-  gap: "15px",
-  flexWrap: "wrap",
-};
+import { SearchByCategory } from "../App";
+import styles from "../Style/CardList.module.css";
 
 const CardList = ({ data }: { data: SearchByCategory }) => {
   console.log(data);
+
   return (
-    <>
-      <section style={styleContainer}>
+    <section className={styles.wrapper_card}>
+      <ul className={styles.wrapper_container_card_product}>
         {data.results.map((product) => (
-          <Card key={product.id} product={product} />
+          <li key={product.id}>
+            <Card product={product} />
+          </li>
         ))}
-      </section>
-    </>
+      </ul>
+    </section>
   );
 };
 
