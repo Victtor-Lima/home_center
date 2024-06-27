4ccf375077a4ea3eaff9537edfefec5c83a95959
João Victor Lima <victtorlima15@gmail.com>
2024-06-27 00:22:36 -0300

    links (with routes) and components that make up the header were inserted

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,32 @@
 import React from "react";
-import NavCategs from "./NavCategs";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import InputSearch from "./InputSearch";
+import styles from "../Style/Header.module.css";
+import logo from "../../public/logo.svg";
+import icfavorite from "../../public/icon_favorite.svg";
+import iccart from "../../public/icon_cart.svg";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <NavCategs />
+    <header className={styles.header}>
+      <Link to="/" className={styles.link_tohome}>
+        <img className={styles.logo} src={logo} alt="Logo da Home Center" />
+      </Link>
+      <InputSearch />
+      <Link to="/favorite" className={styles.link_tohome}>
+        <img
+          className="centralizado"
+          src={icfavorite}
+          alt="link para favoritados"
+        />
+      </Link>
+      <Link to="/cart" className={styles.link_tohome}>
+        <img
+          className="centralizado"
+          src={iccart}
+          alt="link para carrinho de produtos"
+        />
+      </Link>
     </header>
   );
 };
---
+++ src/Components/InputSearch.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../Style/InputSearch.module.css";
+
+const InputSearch = () => {
+  return <input className={styles.input_search} type="text" />;
+};
+
+export default InputSearch;
